692b0ea01b9efd4d0e86aa1803dbb43765ffedbd
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-07-01 18:10:41 +0600

    user registraton serializer create

---
+++ backend/Miss_Event/Account/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from models import CustomUser
+from django.contrib.auth.password_validation import validate_password
+
+class CustomUserRegistrationSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'phone', 'image', 'address', 'password', 'password1']
+        
+        extra_kwargs = {
+            'first_name':{'required':True},
+            'last_name':{'required':True},
+            'username':{'requied':True},
+            'email':{'required',True},
+            'password':{'write_only':True},
+        }
+        
+    def validate(self, attrs):
+        password = attrs.get('password')
+        
+        if password != attrs.get('password1'):
+            raise serializers.ValidationError({'password':'Password did not match'})
+        validate_password(password)
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password1')
+        password = validated_data.pop('password1')
+        
+        user = CustomUser.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+        
