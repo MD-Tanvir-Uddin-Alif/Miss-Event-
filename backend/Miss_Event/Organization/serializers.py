ead460d4289a5689e052a600681bfad78996d9a8
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-07-06 15:50:37 +0600

    organization serializer and view created for registration

--- backend/Miss_Event/Organization/serializers.py
+++ backend/Miss_Event/Organization/serializers.py
@@ -1,1 +1,52 @@
+from rest_framework import serializers
 from .models import OrganizationModel
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class OrganizationRegistrationSerializer(serializers.ModelSerializer):
+    username  = serializers.CharField(write_only=True, required=True)
+    password  = serializers.CharField(write_only=True, required=True)
+    password1  = serializers.CharField(write_only=True, required=True)
+    class Meta:
+        model = OrganizationModel
+        fields = ['id', 'organization', 'address1', 'address2', 'phone', 'link', 'email', 'descriptoin', 'logo', 'username', 'password', 'password1']
+        
+        extra_kwargs = {
+            'username':{'required':True},
+            'email':{'required':True},
+            'phone':{'required':True},
+            'password':{'required':True},
+            'organization':{'required':True},
+            'address1':{'requored':True},
+        }
+    
+    def validate(self, attrs):
+        password = attrs.get('password')
+        
+        if password != attrs.get('password1'):
+            raise serializers.ValidationError({'password':'Password did not match'})
+        validate_password(password)
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password1')       
+        username = validated_data.pop('username')
+        email = validated_data.get('email')
+        password = validated_data.pop('password')
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            is_organizer=True
+        )
+        
+        organization = OrganizationModel.objects.create(
+            organizer=user,
+            **validated_data,
+        )
+        
+        return organization
--- backend/Miss_Event/Organization/views.py
+++ backend/Miss_Event/Organization/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .serializers import OrganizationRegistrationSerializer
+from .models import OrganizationModel
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 # Create your views here.
+
+class OrganizationRegisterView(CreateAPIView):
+    queryset = OrganizationModel.objects.all()
+    serializer_class = OrganizationRegistrationSerializer
+    permission_classes = [AllowAny]
