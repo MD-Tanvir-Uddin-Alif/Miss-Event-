16dd93254e318561149de85145f18671a1706708
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-07-10 17:45:23 +0600

    email send funcation implemented

---
+++ backend/Miss_Event/Utils/email_send_util.py
@@ -0,0 +1,11 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_event_email(subject, message, recipient_email):
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=recipient_email,
+        fail_silently=False
+    )
