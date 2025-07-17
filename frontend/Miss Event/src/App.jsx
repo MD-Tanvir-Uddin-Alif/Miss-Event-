272b7de365528e1eef0d3b39318b155954b926a9
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-07-17 21:35:57 +0600

    Router Navlink added and make some changes in Design

--- frontend/Miss Event/src/App.jsx
+++ frontend/Miss Event/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import Navbar from './assets/components/Navbar'
+import { HashRouter, Routes,Route } from 'react-router-dom'
+import Home from './assets/components/Home'
 
 function App() {
 
   return (
     <div>
-      <Navbar/>
+      <HashRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
--- frontend/Miss Event/src/assets/components/Navbar.jsx
+++ frontend/Miss Event/src/assets/components/Navbar.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-        <nav class="bg-white shadow sticky top-0 z-10">
-            <div class="mx-auto flex items-center justify-between px-6 py-4">
-                <div class="flex items-center space-x-3">
-                <img class="h-10 w-10" src="./login-removebg-preview.png" alt="Logo" />
-                <span class="text-black text-xl font-bold">Miss Event!</span>
-                </div>
+      <nav className="bg-white shadow sticky top-0 z-10">
+        <div className="mx-auto flex items-center justify-between px-6 py-4">
+          <div className="flex items-center space-x-3">
+            <img className="h-10 w-10" src="./login-removebg-preview.png" alt="Logo" />
+            <span className="text-black text-xl font-bold">Miss Event!</span>
+          </div>
 
-                <div class="flex items-center space-x-6">
-                <a href="#" class="text-black font-semibold hover:underline">Home</a>
-                <a href="#" class="text-black font-semibold hover:underline">Explore</a>
-                </div>
-
-                <div>
-                <button class="rounded-md border border-black px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white transition">
-                    Login
-                </button>
-                </div>
-            </div>
-        </nav>
+          <div className="flex items-center space-x-6">
+            <NavLink to='/'  className={({isActive})=>`text-black font-semibold ${isActive? 'underline':''}`}>Home</NavLink>
+            <a href="#" className="text-black font-semibold hover:underline">Explore</a>
+            <button className="rounded-md border border-black px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white transition">
+              Login
+            </button>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
