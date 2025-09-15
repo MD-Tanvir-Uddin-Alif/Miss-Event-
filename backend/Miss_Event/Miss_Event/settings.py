6f60fa4b5ba1dd194a833afd857dfb1cb9ceceb2
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-09-15 12:46:36 +0600

    postgrace db connected

--- backend/Miss_Event/Miss_Event/settings.py
+++ backend/Miss_Event/Miss_Event/settings.py
@@ -131,12 +131,11 @@ DATABASES = {
 }
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600
-#     )
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
