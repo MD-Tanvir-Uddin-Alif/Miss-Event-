376c6bcccee0b1560ae43ff9e4fc282e06a3cad9
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-09-16 19:04:00 +0600

    image .6

--- backend/Miss_Event/Events/serializers.py
+++ backend/Miss_Event/Events/serializers.py
@@ -12,7 +12,7 @@ class EventSerializer(serializers.ModelSerializer):
         fields = [
             "id", "organization", "organization_name",
             "title", "description", "start_time", "end_time",
-            "location", "banner_url" ,"capacity", "created_at", "updated_at"
+            "location", "banner_url","banner" ,"capacity", "created_at", "updated_at"
         ]
         read_only_fields = ["organization", "organization_name", "created_at", "updated_at"]
     
